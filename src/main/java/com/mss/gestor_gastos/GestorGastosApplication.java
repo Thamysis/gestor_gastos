1d50590ab79e6302cfdb73a360f82174fe382fef
anleefc <anlee@usp.br>
2025-06-02 15:57:33 -0300

    Desenvolvimento inicial backend

---
+++ src/main/java/com/mss/gestor_gastos/GestorGastosApplication.java
@@ -0,0 +1,13 @@
+package com.mss.gestor_gastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorGastosApplication.class, args);
+	}
+
+}
